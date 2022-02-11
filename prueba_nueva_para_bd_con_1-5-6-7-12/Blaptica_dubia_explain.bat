4774b4b7a60e3277208ea6646e0e8d7eae338bd6
agustinlovrich <68211865+agustinlovrich@users.noreply.github.com>
2022-02-11 15:50:25 -0300

    Add files via upload

---
+++ prueba_nueva_para_bd_con_1-5-6-7-12/Blaptica_dubia_explain.bat
@@ -0,0 +1,2 @@
+java -mx512m -cp "/C:/Users/agust/Downloads/maxent/maxent/maxent.jar;maxent.jar" density.Explain -l C:\Users\agust\Documents\Tesis\maxent\prueba_nueva_con-1-5-6-7-12\Blaptica_dubia.lambdas -c C:\Users\agust\Documents\Tesis\maxent\prueba_nueva_con-1-5-6-7-12\Blaptica_dubia.asc C:\Users\agust\Documents\Tesis\maxent
+@if errorlevel 1 pause
